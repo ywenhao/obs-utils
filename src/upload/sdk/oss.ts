82e1cb2e4a3ca3f10daa3bf3a60dd708f13fcdd6
yuanwenhao <actions.win@foxmail.com>
2026-02-13 17:58:15 +0800

    feat: wip

--- src/upload/sdk/oss.ts
+++ src/upload/sdk/oss.ts
@@ -1,22 +1,20 @@
-import type { UploadConfig } from '../types'
+import type { OssUploadConfig } from '../types'
 import OSS from 'ali-oss'
 import { consola } from 'consola'
 
-function checkConfig(config: UploadConfig) {
-  if (config.oss) {
-    if (
-      !config.oss.region
-      || !config.oss.bucket
-      || !config.oss.accessKeyId
-      || !config.oss.accessKeySecret
-    ) {
-      consola.error(new Error('upload.config.ts 没有正确配置'))
-      process.exit(1)
-    }
+function checkConfig(config: OssUploadConfig) {
+  if (
+    !config.region
+    || !config.bucket
+    || !config.accessKeyId
+    || !config.accessKeySecret
+  ) {
+    consola.error(new Error('upload.config.ts 没有正确配置'))
+    process.exit(1)
   }
 }
 
-function createClient(config: NonNullable<UploadConfig['oss']>) {
+function createClient(config: OssUploadConfig) {
   return new OSS({
     region: config.region,
     bucket: config.bucket,
